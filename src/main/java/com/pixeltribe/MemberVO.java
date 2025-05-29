8cd543952b3360f8ecec18116774fd623fe4c419
Y.X.Lin <xiang10220619@gmail.com>
2025-05-29 15:12:41 +0800

    請列印出我的程式碼

---
+++ src/main/java/com/pixeltribe/MemberVO.java
@@ -0,0 +1,14 @@
+package com.pixeltribe;
+
+public class MemberVO {
+	private Integer memId;
+	private String memName;
+	
+	public MemberVO() {
+		super();
+		}
+	public static void main(String[] args) {
+		System.out.println("玉祥好帥");
+	}
+	
+}
