891ca4828e791f7c77386aecba1bf0790cba7cdf
jimmy <min0301.tw@gmail.com>
2025-05-29 15:12:58 +0800

    今天下雨天，記得帶傘

---
+++ src/main/java/com/pixeltribe/MemberVO.java
@@ -0,0 +1,38 @@
+package com.pixeltribe;
+
+import java.util.Objects;
+
+public class MemberVO {
+	private Integer memId;
+	private String memName;
+	public Integer getMemId() {
+		return memId;
+	}
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(memId, memName);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MemberVO other = (MemberVO) obj;
+		return Objects.equals(memId, other.memId) && Objects.equals(memName, other.memName);
+	}
+	
+	
+
+}
